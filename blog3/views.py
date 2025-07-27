a3e759b13d6423db2931a6d6a73a05b153263b64
Veerasundari-2001 <veerasundari14@gmail.com>
2025-07-27 12:04:52 +0530

    first commit test1

---
+++ blog3/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def home(request):
+    msg='welcome to django'
+    return HttpResponse(msg)
+def index(request):
+    
+    return HttpResponse('<h1>welcometo index page</h1>')
+def fun(request):
+    return render(request,'login.html')
